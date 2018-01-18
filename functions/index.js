31d2da478aeb6d9bda0c7bdff6d6db7a123d3bd6
Dale Nguyen <dungnq@itbox4vn.com>
2018-01-18 10:35:10 -0500

    Added cloud funtions and firebase admin modules

--- functions/index.js
+++ functions/index.js
@@ -14,9 +14,14 @@
  * limitations under the License.
  */
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+ // Import the Firebase SDK for Google Cloud Functions.
+ const functions = require('firebase-functions');
+ // Import and initialize the Firebase Admin SDK.
+ const admin = require('firebase-admin');
+ admin.initializeApp(functions.config().firebase);
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+ // Adds a message that welcomes new users into the chat.
+ 
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
